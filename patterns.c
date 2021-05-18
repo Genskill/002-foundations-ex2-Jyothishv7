85ac60bf37144d6e1dbe6c25df2b6b3213e1fda7
Jyothishv7 <61043734+Jyothishv7@users.noreply.github.com>
2021-05-18 22:31:38 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -1,40 +1,35 @@
-#include <stdio.h>
-#include <cs50.h>
-
-void main()
-
+#include<stdio.h>
+#include<cs50.h>
+int main(void){
+int choice=get_int("Enter type of pattern (1 or 2) ");
+int r=get_int("Number of rows");
+if(choice==1)
 {
- int i,j;
- int pat =get_int("Enter type of pattern (1 or 2)");
- int rows =get_int("Number of rows");
- if(pat == 1)
- {
-  for(i=0; i<rows; ++i)
-  {
-   for(j=i; j< rows; ++j)
+ for(int i=r;i>0;i--)
    {
-     printf("#");
+     for(int j=0;j<i;j++)
+      { 
+       printf("#");
+      }
+      printf("\n");
    }
-   printf("\n");
-  }
  }
-
- else if(pat == 2)
- {
-  for(i=1; i<=rows; ++i)
-   {
-     for(j=i; j<rows; ++j)
+else if(choice==2)
+{
+  for(int i=r;i>0;i--)
+    {
+      for(int j=0;j<r;j++)
        {
-            printf(" ");
+         if(j>=i-1)
+         {
+          printf("#");
+         }
+         else
+         printf(" ");
        }
-        
-     for(j=1; j<=i; ++j)
-        {
-            printf("#");
-        }
-        
-        printf("\n");
-    }
- }
- 
+      printf("\n");
+     }
 }
+else
+ printf("error");
+};
